9680d20201e4fa9a6bc140b97f6b5a183fcb9942
rajduino <rajdeep1996karmakar@gmail.com>
2019-11-28 15:22:16 +0530

    Adding test code

--- Visualiser/main.js
+++ Visualiser/main.js
@@ -1,7 +1,15 @@
-function setup() {
-  // put setup code here
+function setup()
+{
+	createCanvas(windowWidth,windowHeight);
 }
 
-function draw() {
-  // put drawing code here
+function draw()
+{
+	clear();
+	background(200,0,100);
+	ellipse(mouseX,mouseY,50,50);
+}
+function windowResized()
+{
+	resizeCanvas(windowWidth, windowHeight);
 }
