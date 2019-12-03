393d754a98f9e0b09bd67b50158f5024e2bd793a
rajduino <rajdeep1996karmakar@gmail.com>
2019-12-03 20:55:49 +0530

    Added modified selectionSortS() and others!

--- main.js
+++ main.js
@@ -3,7 +3,9 @@ let arr=[]; //array to store the required elements for the required algorithm
 //pageNo: variable to store the current page number
 //algoType: variable to store the selected algorithm type, Searching=1, sorting=2
 //algoNo: variable to store the selected algorithm within a selected algorithm type
-let pageNo, algoType, algoNo;
+let pageNo, algoType, algoNo,sorted,iterator;
+
+let graphWidth,graphHeight,barWidth,max,graphPadLeft,graphPadTop;
 
 function setup()
 {
@@ -11,14 +13,21 @@ function setup()
 	appHeight=(windowHeight<720)?720:windowHeight; //minimum height of the app should be 720 pixels and max should be the windowHeight
 	createCanvas(appWidth,appHeight);
 	pageNo=0;
-	algoType=0;
-	algoNo=0;
+	algoType=2;
+	algoNo=1;
+	generateArray(10);
 }
 
 function draw()
 {
+	graphWidth=appWidth*0.6;
+	graphHeight=appHeight*0.6;
+	barWidth=graphWidth/arr.length;
+	graphPadLeft=appWidth*0.2;
+	graphPadTop=appHeight*0.3;
 	clear();
-	background(200,0,100);
+	background(255);
+	fill(0,200,200);
 	//switch case to maintain required pages with pageNo value
 	switch(pageNo)
 	{
@@ -26,11 +35,29 @@ function draw()
 		
 	break;
 	case 1:
+		if(algoType===1)
+		{
 
+		}
+		else if(algoType===2)
+		{
+			if(algoNo===1)
+			{
+				if(sorted===false)
+				{
+					sorted=selectionSortS(iterator);
+					iterator=iterator+1;
+				}
+			}
+		}
 	break;
 	case 2:
 
 	break;
+	}
+	for(let i=0;i<arr.length;i++)
+	{
+		rect(graphPadLeft+i*barWidth,graphPadTop+(max-arr[i])*graphHeight/max,barWidth,arr[i]*graphHeight/max);
 	}
 }
 function windowResized()
@@ -38,6 +65,14 @@ function windowResized()
 	appWidth=(windowWidth<1280)?1280:windowWidth;
 	appHeight=(windowHeight<720)?720:windowHeight;
 	resizeCanvas(appWidth, appHeight);
+}
+function mousePressed()
+{
+	console.log("called");
+	pageNo=1;
+	sorted=false;
+	iterator=0;
+	//selectionSort();
 }
 function searchingSelected()
 {
@@ -49,17 +84,35 @@ function sortingSelected()
 	pageNo=1;
 	algoType=2;
 }
-function generateArray(let n)
+function generateArray(n)
 {
 	arr=[];
+	max=0;
 	for(let i=0;i<n;i++)
 	{
-		append(arr,random(1,100));
+		append(arr,(int)(100*random()));
+		if(max<arr[i])
+			max=arr[i];
 	}
 }
+/*
 function selectionSort()
 {
+	print(arr);
+	let graphWidth,graphHeight,barWidth,max,graphPadLeft,graphPadTop;
+	graphWidth=appWidth*0.8;
+	graphHeight=appHeight*0.8;
+	barWidth=graphWidth/arr.length;
+	graphPadLeft=appWidth*0.1;
+	graphPadTop=appHeight*0.1;
 	let m;
+	m=0;
+	for(let i=0;i<arr.length;i++)
+	{
+		if(arr[m]<arr[i])
+			m=i;
+	}
+	max=arr[m];
 	for(let i=0;i<arr.length-1;i++)
 	{
 		m=i;
@@ -75,4 +128,34 @@ function selectionSort()
 			arr[i]=t;
 		}
 	}
+}
+*/
+function selectionSortS(d)
+{
+	let y,m,t,n;
+	myDelay(500);
+	n=arr.length;
+	if(d>=n)
+		return true;
+	m=d;
+	for(y=d+1;y<n;y++)
+	{
+		if(arr[m]>arr[y])
+			m=y;
+	}
+	if(m!=d)
+	{
+		t=arr[m];
+		arr[m]=arr[d];
+		arr[d]=t;
+	}
+	if(d<(n-1))
+		return false;
+	else
+		return true;
+}
+function myDelay(deltaT)
+{
+	let startT=millis();
+	while(millis() < startT + deltaT);
 }
