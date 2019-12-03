a0ce91033af4b2b052cf6fedc6a842ea6f06c66e
rajduino <rajdeep1996karmakar@gmail.com>
2019-12-03 21:08:50 +0530

    resolved merge

--- main.js
+++ main.js
@@ -66,7 +66,6 @@ function windowResized()
 	appHeight=(windowHeight<720)?720:windowHeight;
 	resizeCanvas(appWidth, appHeight);
 }
-<<<<<<< HEAD
 function mousePressed()
 {
 	console.log("called");
@@ -75,8 +74,6 @@ function mousePressed()
 	iterator=0;
 	//selectionSort();
 }
-=======
->>>>>>> 12043e135563ee4c7514afa0a538d577e39bf4e1
 function searchingSelected()
 {
 	pageNo=1;
@@ -87,7 +84,6 @@ function sortingSelected()
 	pageNo=1;
 	algoType=2;
 }
-<<<<<<< HEAD
 function generateArray(n)
 {
 	arr=[];
@@ -117,7 +113,6 @@ function selectionSort()
 			m=i;
 	}
 	max=arr[m];
-=======
 function generateArray(let n)
 {
 	arr=[];
@@ -129,7 +124,6 @@ function generateArray(let n)
 function selectionSort()
 {
 	let m;
->>>>>>> 12043e135563ee4c7514afa0a538d577e39bf4e1
 	for(let i=0;i<arr.length-1;i++)
 	{
 		m=i;
@@ -145,7 +139,6 @@ function selectionSort()
 			arr[i]=t;
 		}
 	}
-<<<<<<< HEAD
 }
 */
 function selectionSortS(d)
@@ -176,6 +169,4 @@ function myDelay(deltaT)
 {
 	let startT=millis();
 	while(millis() < startT + deltaT);
-=======
->>>>>>> 12043e135563ee4c7514afa0a538d577e39bf4e1
 }
