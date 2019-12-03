0e3450591b2fd2f7c81599dbd33b5e4725155d7d
Anirban Dey <anirbandey303@gmail.com>
2019-12-03 20:48:07 +0530

    implemented sorting page with buttons

--- main.js
+++ main.js
@@ -10,9 +10,12 @@ let arr = []; //array to store the required elements for the required algorithm
 //subHeading: variable that can be used to manipulate the text of the sub-heading in index page
 //firstPageBtns: variable to select all buttons on first page(selects entire div)
 //backBtn: variable that will link to previous page
+//selectionSort, mergeSort, bubbleSort: selecting the sorting buttons from html and storing inside them
 let pageNo, algoType, algoNo;
 let sortBtn, searchBtn, treeBtn, firstPageBtns, backBtn;
 var heading, subheading;
+var sortPageBtns;
+var selectionSort, mergeSort, bubbleSort;
 
 function setup() {
     appWidth = (windowWidth < 1280) ? 1280 : windowWidth; //minimum width of the app should be 1280 pixels and max should be the windowWidth
@@ -22,10 +25,16 @@ function setup() {
     algoType = 0;
     algoNo = 0;
     firstPageBtns = select('#btns'); //storing all btns in of first page in a variable
+    sortPageBtns = select('#allSortButtons'); //storing the div containing all sort related buttons
     backBtn = createButton('Back'); //creating a back button
+    backBtn.addClass('btn');
+    backBtn.addClass('btn-danger');
+    selectionSort = select('#selectionSort');
+    mergeSort = select('#mergeSort');
+    bubbleSort = select('#bubbleSort');
     showLandingPage(); //show first page
 }
-
+/*------------ Landing Page------------------*/
 function showLandingPage() {
     heading = select("#heading"); //Select heading on index page
     subHeading = select("#subheading"); //Select sub-heading part on index page
@@ -33,24 +42,35 @@ function showLandingPage() {
     subHeading.html("Welcome! Watch animations of all Data Structures and Algorithms in Real time"); //text of subheading on first page
     firstPageBtns.show(); //show all buttons of first page
     backBtn.hide(); //Hiding back button on first page
+    sortPageBtns.hide() //hiding sort page buttons
     sortBtn = select('#sort');
     sortBtn.mousePressed(showSortingPage);
-
 }
+/* ------- End of Landing Page ----------*/
 
+/*--------------------- Sorting Selected Page ----------------*/
 function showSortingPage() {
+    // pageNo = 1;
+    // algoType = 2;
     //changing display text of headings
     heading.html("Sorting Visualizer"); //Text of heading on first page
     subHeading.html("Let's visualize some sorting algorithms!!!"); //text of subheading on first page
     pageNo = 1; //Setting variable value to 1 as user chose Sorting
-    //hiding the buttons of first page
-    firstPageBtns.hide();
+
+    firstPageBtns.hide(); //hiding the buttons of first page
+    sortPageBtns.show(); //showing sort page buttons
 
     //adding a back button
     backBtn.show()
     backBtn.mousePressed(showLandingPage);
 
+    //calling each function when clicked on given algorithm
+    selectionSort.mousePressed(selectionSortSelected);
+    mergeSort.mousePressed(mergeSortSelected);
+    bubbleSort.mousePressed(bubbleSortSelected);
+
 }
+/* -------End of Sorting Page --------------*/
 
 function draw() {
     clear();
@@ -73,4 +93,63 @@ function windowResized() {
     appWidth = (windowWidth < 1280) ? 1280 : windowWidth;
     appHeight = (windowHeight < 720) ? 720 : windowHeight;
     resizeCanvas(appWidth, appHeight);
+}
+
+function selectionSortSelected() {
+    pageNo = 1;
+    algoType = 1;
+
+    heading.html("Selection Sort"); //Text of heading on first page
+    /* Making button look selected */
+    selectionSort.addClass("active");
+    bubbleSort.removeClass("active");
+    mergeSort.removeClass("active");
+    /* Making button look selected  END*/
+
+
+}
+
+function mergeSortSelected() {
+    pageNo = 1;
+    algoType = 2;
+    heading.html("Merge Sort"); //Text of heading on first page
+    /* Making button look selected */
+    selectionSort.removeClass("active");
+    bubbleSort.removeClass("active");
+    mergeSort.addClass("active");
+    /* Making button look selected  END*/
+}
+
+function bubbleSortSelected() {
+    pageNo = 1;
+    algoType = 3;
+    heading.html("Bubble Sort"); //Text of heading on first page
+    /* Making button look selected */
+    selectionSort.removeClass("active");
+    bubbleSort.addClass("active");
+    mergeSort.removeClass("active");
+    /* Making button look selected  END*/
+}
+
+// function generateArray(let n) {
+//     arr = [];
+//     for (let i = 0; i < n; i++) {
+//         append(arr, (int)(100 * random()));
+//     }
+// }
+
+function selectionSort() {
+    let m;
+    for (let i = 0; i < arr.length - 1; i++) {
+        m = i;
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[m] > arr[j])
+                m = j;
+        }
+        if (m != i) {
+            let t = arr[m];
+            arr[m] = arr[i];
+            arr[i] = t;
+        }
+    }
 }
